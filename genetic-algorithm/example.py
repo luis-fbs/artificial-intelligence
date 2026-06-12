ba4aa76890f2c627a18f34cb2b693d14e6aeea27
Luis Felipe Baptista da  Silva <130479694+luis-fbs@users.noreply.github.com>
2026-06-11 22:31:19 -0300

    Genetic algorithm (#1)
    * Implementation of a Genetic Algorithm.

---
+++ genetic-algorithm/example.py
@@ -0,0 +1,28 @@
+import math
+import random
+
+from genetic_algorithm import genetic_algorithm
+
+
+def bin_to_float(x):
+    min, max, n_bits = -100, 100, 22
+    distance = max - min
+    step = distance / (2 ** n_bits - 1)
+    return min + x * step
+
+def decode_value(value):
+    x_bin = value >> 22
+    y_bin = ( (1 << 22) -1 ) & value
+    x = bin_to_float(x_bin)
+    y = bin_to_float(y_bin)
+    return x, y
+
+def fitness(value):
+    x, y = decode_value(value)
+    t = x*x + y*y
+    return 0.5 - (pow(math.sin(math.sqrt(t)), 2)- 0.5) / pow(1+0.001*t, 2)
+
+
+population = [random.getrandbits(44) for _ in range(100)]
+i, f = genetic_algorithm(population, fitness, 44)
+print(decode_value(i), f)
---
+++ genetic-algorithm/genetic_algorithm.py
@@ -0,0 +1,84 @@
+import math
+import random
+
+
+def spin_roulette(cumulative_fitness):
+    roulette = random.random() * cumulative_fitness[-1]
+    for index, value in enumerate(cumulative_fitness):
+        if value >= roulette: break
+    return index
+
+def generate_pair(cumulative_fitness):
+    index_individual1 = spin_roulette(cumulative_fitness)
+    index_individual2 = index_individual1
+
+    while index_individual2 == index_individual1:
+        index_individual2 = spin_roulette(cumulative_fitness)
+    return index_individual1, index_individual2
+
+def selection(population, fitness_function):
+    best_individual = population[0]
+    best_fitness = fitness_function(population[0])
+
+    total = 0
+    cumulative_fitness = []
+    for individual in population:
+        individual_fitness = fitness_function(individual)
+        if individual_fitness > best_fitness:
+            best_individual = individual
+            best_fitness = individual_fitness
+
+        total += individual_fitness
+        cumulative_fitness.append(total)
+
+    population_size = len(population)
+    selected_pairs = []
+    for _ in range(math.ceil(population_size/2)):
+        index_individual1, index_individual2 = generate_pair(cumulative_fitness)
+        pair = (population[index_individual1], population[index_individual2])
+        selected_pairs.append(pair)
+
+    return selected_pairs, best_individual, best_fitness
+
+def crossover(pair, n_bits,rate):
+    if random.random() > rate:
+        return pair
+
+    cut = random.randint(1, n_bits-1)
+    head_mask = (1 << cut) - 1
+    mask = head_mask << (n_bits - cut)
+
+    sun1 = (pair[0] & mask) | (pair[1] & ~mask)
+    sun2 = (pair[1] & mask) | (pair[0] & ~mask)
+
+    return sun1, sun2
+
+def mutate(individual, n_bits,rate):
+    if random.random() > rate:
+        return individual
+
+    position = random.randint(0, n_bits-1)
+    return individual ^ (1 << position)
+
+def genetic_algorithm(population, fitness_function, n_bits, crossover_rate = 0.65, mutation_rate = 0.008):
+    best_individual, best_fitness = population[0], fitness_function(population[0])
+    generations_without_improvement = 0
+    while generations_without_improvement < 50:
+        selected_pairs, population_best_individual, population_best_fitness = selection(population, fitness_function)
+
+        if population_best_fitness > best_fitness:
+            best_individual = population_best_individual
+            best_fitness = population_best_fitness
+            generations_without_improvement = 0
+        else:
+            generations_without_improvement += 1
+
+        next_generation = []
+        for pair in selected_pairs:
+            for individual in crossover(pair, n_bits, crossover_rate):
+                mutated_individual = mutate(individual, n_bits, mutation_rate)
+                next_generation.append(mutated_individual)
+
+        population = next_generation
+
+    return best_individual, best_fitness
